6cea64527dc765250411191fb883a316c0635cb7
stuart brown <stuart.c.brown@hsbcgroup.com>
2015-06-03 09:24:03 +0100

    init

---
+++ java_ed/src/main/java/org/stubrown/java_ed/App.java
@@ -0,0 +1,13 @@
+package org.stubrown.java_ed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
